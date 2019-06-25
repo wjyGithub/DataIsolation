0804ac3c7b35d3980d95e4e55f043389f2038764
wjy <jianyuan.wei@hand-china.com>
2019-06-25 22:53:03 +0800

    代码优化

---
+++ src/main/java/com/data/isolation/interceptor/DruidInterceptor.java
@@ -0,0 +1,48 @@
+package com.data.isolation.interceptor;
+
+import com.data.isolation.parser.DruidUtil;
+import org.apache.ibatis.executor.statement.StatementHandler;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Signature;
+import org.springframework.stereotype.Component;
+
+import java.lang.reflect.Field;
+import java.sql.Connection;
+import java.util.Properties;
+
+/**
+ * Created by jianyuan.wei@hand-china.com
+ * on 2019/6/25 20:03.
+ */
+@Component
+@Intercepts({@Signature(type = StatementHandler.class, method = "prepare", args = {Connection.class, Integer.class})})
+public class DruidInterceptor implements Interceptor {
+
+
+    @Override
+    public Object intercept(Invocation invocation) throws Throwable {
+        StatementHandler statementHandler = (StatementHandler) invocation.getTarget();
+        BoundSql boundSql = statementHandler.getBoundSql();
+        //获取到原始sql语句
+        String sql = boundSql.getSql();
+        //通过反射修改sql语句
+        Field field = boundSql.getClass().getDeclaredField("sql");
+        field.setAccessible(true);
+        DruidUtil.addWhereForSelectSql(sql);
+        field.set(boundSql,field);
+        return invocation.proceed();
+    }
+
+    @Override
+    public Object plugin(Object o) {
+        return null;
+    }
+
+    @Override
+    public void setProperties(Properties properties) {
+
+    }
+}
--- src/main/java/com/data/isolation/parser/SQLUtil.java
+++ src/main/java/com/data/isolation/parser/SQLUtil.java
@@ -38,13 +38,13 @@ public class SQLUtil {
     public static final void processPlainSelect(SQLSelectQueryBlock queryBlock) {
         //处理sql查询的字段
         List<SQLSelectItem> selectColumns = queryBlock.getSelectList();
-        for(SQLSelectItem selectItem : selectColumns) {
+        for (SQLSelectItem selectItem : selectColumns) {
             processQueryInSelectItem(selectItem);
         }
 
         //处理表
         SQLTableSource tables = queryBlock.getFrom();
-        processTableSource(tables,queryBlock);
+        processTableSource(tables, queryBlock);
 
         //处理where条件里面的子查询
         SQLExpr where = queryBlock.getWhere();
@@ -57,32 +57,33 @@ public class SQLUtil {
 
     /**
      * 处理表
+     *
      * @param tableSource
      * @param queryBlock
      */
-    public static final void processTableSource(SQLTableSource tableSource,SQLSelectQueryBlock queryBlock) {
-        if(tableSource instanceof SQLExprTableSource) {
+    public static final void processTableSource(SQLTableSource tableSource, SQLSelectQueryBlock queryBlock) {
+        if (tableSource instanceof SQLExprTableSource) {
             //单表
-            SQLExprTableSource exprTableSource = (SQLExprTableSource)tableSource;
-            processExprTableSource(exprTableSource,queryBlock);
-        } else if(tableSource instanceof SQLJoinTableSource) {
-            SQLJoinTableSource joinTableSource = (SQLJoinTableSource)tableSource;
-            processJoinTableSource(joinTableSource,queryBlock);
+            SQLExprTableSource exprTableSource = (SQLExprTableSource) tableSource;
+            processExprTableSource(exprTableSource, queryBlock);
+        } else if (tableSource instanceof SQLJoinTableSource) {
+            SQLJoinTableSource joinTableSource = (SQLJoinTableSource) tableSource;
+            processJoinTableSource(joinTableSource, queryBlock);
         } else if (tableSource instanceof SQLSubqueryTableSource) {
             //子查询,不包含union/union all
-            SQLSubqueryTableSource subqueryTableSource = (SQLSubqueryTableSource)tableSource;
+            SQLSubqueryTableSource subqueryTableSource = (SQLSubqueryTableSource) tableSource;
             SQLSelect select = subqueryTableSource.getSelect();
             SQLSelectQuery query = select.getQuery();
-            if(query instanceof SQLSelectQueryBlock) {
-                SQLSelectQueryBlock selectQueryBlock = (SQLSelectQueryBlock)query;
+            if (query instanceof SQLSelectQueryBlock) {
+                SQLSelectQueryBlock selectQueryBlock = (SQLSelectQueryBlock) query;
                 processPlainSelect(selectQueryBlock);
-            } else if(query instanceof SQLUnionQuery) {
-                SQLUnionQuery unionQuery = (SQLUnionQuery)query;
+            } else if (query instanceof SQLUnionQuery) {
+                SQLUnionQuery unionQuery = (SQLUnionQuery) query;
                 processUnionSelect(unionQuery);
             }
         } else if (tableSource instanceof SQLUnionQueryTableSource) {
             //子查询 包含union/union all
-            SQLUnionQueryTableSource unionQueryTable = (SQLUnionQueryTableSource)tableSource;
+            SQLUnionQueryTableSource unionQueryTable = (SQLUnionQueryTableSource) tableSource;
             SQLUnionQuery union = unionQueryTable.getUnion();
             processUnionSelect(union);
         }
@@ -90,94 +91,91 @@ public class SQLUtil {
 
     /**
      * 处理单表
+     *
      * @param tableSource
      * @param queryBlock
      */
-    public static final void processExprTableSource(SQLExprTableSource tableSource,SQLSelectQueryBlock queryBlock) {
+    public static final void processExprTableSource(SQLExprTableSource tableSource, SQLSelectQueryBlock queryBlock) {
         String columnName = getColumnName(tableSource);
-        addWhere(columnName,queryBlock);
+        addWhere(columnName, queryBlock);
     }
 
     /**
      * 处理left/inner/, 不处理right
+     *
      * @param joinTableSource
      * @param queryBlock
      */
-    public static final void processJoinTableSource(SQLJoinTableSource joinTableSource,SQLSelectQueryBlock queryBlock) {
+    public static final void processJoinTableSource(SQLJoinTableSource joinTableSource, SQLSelectQueryBlock queryBlock) {
         SQLTableSource rightTableSource = joinTableSource.getRight();
-        if(rightTableSource instanceof SQLSubqueryTableSource) {
-            SQLSubqueryTableSource subqueryTableSource = (SQLSubqueryTableSource)rightTableSource;
+        if (rightTableSource instanceof SQLSubqueryTableSource) {
+            SQLSubqueryTableSource subqueryTableSource = (SQLSubqueryTableSource) rightTableSource;
             SQLSelect select = subqueryTableSource.getSelect();
             SQLSelectQuery query = select.getQuery();
-            if(query instanceof SQLSelectQueryBlock) {
-                SQLSelectQueryBlock selectQueryBlock = (SQLSelectQueryBlock)query;
+            if (query instanceof SQLSelectQueryBlock) {
+                SQLSelectQueryBlock selectQueryBlock = (SQLSelectQueryBlock) query;
                 processPlainSelect(selectQueryBlock);
             } else if (query instanceof SQLUnionQuery) {
-                SQLUnionQuery unionQuery = (SQLUnionQuery)query;
+                SQLUnionQuery unionQuery = (SQLUnionQuery) query;
                 processUnionSelect(unionQuery);
             }
-//            if(joinTableSource.getJoinType() != SQLJoinTableSource.JoinType.COMMA) {
             addConditionForRight(joinTableSource);
-//            }
-        } else if(rightTableSource instanceof SQLExprTableSource) {
-            //单表
-            SQLExprTableSource exprTableSource = (SQLExprTableSource)rightTableSource;
-            String rightCondition = getColumnName(exprTableSource);
-//            if(joinTableSource.getJoinType() == SQLJoinTableSource.JoinType.COMMA) {
-//                processExprTableSource(exprTableSource,queryBlock);
-//            } else {
-                addAndCondition(rightCondition, joinTableSource);
-                addConditionForRight(joinTableSource);
-//            }
 
+        } else if (rightTableSource instanceof SQLExprTableSource) {
+            //单表
+            SQLExprTableSource exprTableSource = (SQLExprTableSource) rightTableSource;
+            String rightCondition = getColumnName(exprTableSource);
+            addAndCondition(rightCondition, joinTableSource);
+            addConditionForRight(joinTableSource);
         }
         SQLTableSource leftTableSource = joinTableSource.getLeft();
         //子查询
-        if(leftTableSource instanceof SQLSubqueryTableSource) {
-            SQLSubqueryTableSource subqueryTableSource = (SQLSubqueryTableSource)leftTableSource;
+        if (leftTableSource instanceof SQLSubqueryTableSource) {
+            SQLSubqueryTableSource subqueryTableSource = (SQLSubqueryTableSource) leftTableSource;
             SQLSelect select = subqueryTableSource.getSelect();
             SQLSelectQuery query = select.getQuery();
-            if(query instanceof SQLSelectQueryBlock) {
+            if (query instanceof SQLSelectQueryBlock) {
                 SQLSelectQueryBlock selectQueryBlock = (SQLSelectQueryBlock) query;
                 processPlainSelect(selectQueryBlock);
-            } else if(query instanceof SQLUnionQuery) {
-                SQLUnionQuery unionQuery = (SQLUnionQuery)query;
+            } else if (query instanceof SQLUnionQuery) {
+                SQLUnionQuery unionQuery = (SQLUnionQuery) query;
                 processUnionSelect(unionQuery);
             }
-        } else if(leftTableSource instanceof SQLJoinTableSource) {
+        } else if (leftTableSource instanceof SQLJoinTableSource) {
             //left/inner/,
-            SQLJoinTableSource joinTable = (SQLJoinTableSource)leftTableSource;
-            processJoinTableSource(joinTable,queryBlock);
-        } else if(leftTableSource instanceof SQLExprTableSource) {
+            SQLJoinTableSource joinTable = (SQLJoinTableSource) leftTableSource;
+            processJoinTableSource(joinTable, queryBlock);
+        } else if (leftTableSource instanceof SQLExprTableSource) {
             //单表
-            SQLExprTableSource exprTableSource = (SQLExprTableSource)leftTableSource;
-            processExprTableSource(exprTableSource,queryBlock);
+            SQLExprTableSource exprTableSource = (SQLExprTableSource) leftTableSource;
+            processExprTableSource(exprTableSource, queryBlock);
         }
     }
 
     public static final void addConditionForRight(SQLJoinTableSource joinTableSource) {
         SQLTableSource right = joinTableSource.getRight();
-        if(right instanceof  SQLExprTableSource) {
+        if (right instanceof SQLExprTableSource) {
             String leftColumnName = getColumnName((SQLExprTableSource) right);
-            addAndCondition(leftColumnName,joinTableSource);
+            addAndCondition(leftColumnName, joinTableSource);
         }
     }
 
     /**
      * 为左连接添加On条件
+     *
      * @param joinTableSource join连接
      */
     public static final void addConditionForLeft(SQLJoinTableSource joinTableSource) {
         SQLTableSource leftTableSource = joinTableSource.getLeft();
-        if(leftTableSource instanceof SQLExprTableSource) {
+        if (leftTableSource instanceof SQLExprTableSource) {
             String leftColumnName = getColumnName((SQLExprTableSource) leftTableSource);
-            addAndCondition(leftColumnName,joinTableSource);
-        } else if(leftTableSource instanceof SQLJoinTableSource) {
-            SQLJoinTableSource joinTable = (SQLJoinTableSource)leftTableSource;
+            addAndCondition(leftColumnName, joinTableSource);
+        } else if (leftTableSource instanceof SQLJoinTableSource) {
+            SQLJoinTableSource joinTable = (SQLJoinTableSource) leftTableSource;
             SQLTableSource right = joinTable.getRight();
-            if(right instanceof SQLExprTableSource) {
-                String leftColumnName = getColumnName((SQLExprTableSource)right);
-                addAndCondition(leftColumnName,joinTableSource);
+            if (right instanceof SQLExprTableSource) {
+                String leftColumnName = getColumnName((SQLExprTableSource) right);
+                addAndCondition(leftColumnName, joinTableSource);
             }
         }
     }
@@ -186,20 +184,20 @@ public class SQLUtil {
         return StringUtils.isEmpty(tableSource.getAlias()) ? "organization_id" : tableSource.getAlias() + ".organization_id";
     }
 
-    public static final void addWhere(String columnName,SQLSelectQueryBlock queryBlock){
-        if(isIn) {
-            queryBlock.addWhere(WhereUtil.getInExpr(columnName,DataUtil.getOrganizationIds()));
+    public static final void addWhere(String columnName, SQLSelectQueryBlock queryBlock) {
+        if (isIn) {
+            queryBlock.addWhere(WhereUtil.getInExpr(columnName, DataUtil.getOrganizationIds()));
         } else {
-            queryBlock.addWhere(WhereUtil.getEqualTo(columnName,DataUtil.getOrganizationId()));
+            queryBlock.addWhere(WhereUtil.getEqualTo(columnName, DataUtil.getOrganizationId()));
         }
     }
 
-    public static final void addAndCondition(String columnName,SQLJoinTableSource joinTableSource) {
+    public static final void addAndCondition(String columnName, SQLJoinTableSource joinTableSource) {
         SQLExpr condition = joinTableSource.getCondition();
-        if(isIn) {
-            joinTableSource.addConditionn(WhereUtil.getInExpr(columnName,DataUtil.getOrganizationIds()));
+        if (isIn) {
+            joinTableSource.addConditionn(WhereUtil.getInExpr(columnName, DataUtil.getOrganizationIds()));
         } else {
-            joinTableSource.addConditionn(WhereUtil.getEqualTo(columnName,DataUtil.getOrganizationId()));
+            joinTableSource.addConditionn(WhereUtil.getEqualTo(columnName, DataUtil.getOrganizationId()));
         }
 
     }
@@ -224,7 +222,7 @@ public class SQLUtil {
             processPlainSelect(leftSelectQueryBlock);
         } else if (leftSelectQuery instanceof SQLUnionQuery) {
             //含有union的sql
-            SQLUnionQuery leftUnionQuery = (SQLUnionQuery)leftSelectQuery;
+            SQLUnionQuery leftUnionQuery = (SQLUnionQuery) leftSelectQuery;
             processUnionSelect(leftUnionQuery);
         }
     }
